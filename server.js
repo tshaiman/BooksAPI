760c3a4ac625a46f8749e4b3025013a5a48e07b1
tshaiman <tomer.shaiman@gmail.com>
2015-09-04 17:51:10 +0300

    add router module
    add router module

--- server.js
+++ server.js
@@ -1,41 +1,29 @@
 ﻿var express = require('express'),
-    mongoose = require('mongoose'),
+	mongoose = require('mongoose'),
 	bodyParser = require('body-parser');
 
 
-	 
+
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
 var Book = require('./models/bookModel');
 var app = express();
-var port = process.env.PORT ||  3000;
+var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
-bookRouter.route('/Books')
-		.post(function(req,res){
-			var book = new Book(req.body);
-			book.save();
-			res.status(201).send(book);
-		})
-		.get(function(req,res){
-			  Book.find(function(err,books){
-				  if(err)
-					  res.status(500).send(err);
-				  else
-				  	res.json(books);
-			  }) 
-			   
-		   });
+var bookRouter = require('./Routes/bookRoutes')(Book);
 
-app.use('/api',bookRouter);
 
-app.get('/', function (req, res) {
+app.use('/api/books', bookRouter);
+
+app.get('/', function(req, res) {
 	res.send('Welcome to my API !');
 });
 
-app.listen(port, function () {
+app.listen(port, function() {
 	console.log('Gulp is running on port ' + port);
 });
